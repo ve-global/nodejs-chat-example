167b72e86e3d46dc5f162c6fd6db13fde9fc26b0
Paul Dan Pruteanu <paulpruteanu@yahoo.com>
2015-08-04 14:00:22 +0100

    adjust doc and variable names

--- server.js
+++ server.js
@@ -11,42 +11,51 @@ var messages = [],
 // Handle the client connecting to the socket.
 io.on('connection', function(socket) {
 
-    var member = 'Guest' + socket.conn.id.substring(0, 4),
+    var
+    // Provide a name to the newly connected client, based on the socket connection id.
+        client = 'Guest' + socket.conn.id.substring(0, 4),
+    // Set the current date, user-friendly formatted.
         date = moment().format('hh:mm:ss'),
+    // New connection message to be broadcasted to clients.
         messageData = {
-            member: member,
+            client: client,
             date: date,
             message: 'Hey there, I just joined the chat!'
         };
 
-    console.log(member + ' connected.');
+    console.log(client + ' connected.');
 
     messages.push(messageData);
 
+    // Broadcast the current messages to the newly connected client.
     socket.emit('currentMessages', messages);
+    // Broadcast to all client listeners (except the newly connected client) about the new joined client.
     socket.broadcast.emit('newMessage', messageData);
 
+    // Receive new messages from client.
     socket.on('newMessage', function(message) {
 
-        console.log(member + ' said ', message);
+        console.log(client + ' said ', message);
 
-        var date = moment().format('hh:mm:ss');
-        var messageData = {
-            member: member,
-            date: date,
-            message: message
-        };
+        var date = moment().format('hh:mm:ss'),
+            messageData = {
+                client: client,
+                date: date,
+                message: message
+            };
 
         messages.push(messageData);
+        // Announce the new message to all participants (including the current client).
         io.sockets.emit('newMessage', messageData);
 
     });
 
+    // Handle a disconnected client.
     socket.on('disconnect', function() {
 
         var date = moment().format('hh:mm:ss');
         var messageData = {
-            member: member,
+            client: client,
             date: date,
             message: 'Bye-bye!'
         };
@@ -54,12 +63,13 @@ io.on('connection', function(socket) {
         messages.push(messageData);
         io.sockets.emit('newMessage', messageData);
 
-        console.log(member + ' disconnected.');
+        console.log(client + ' disconnected.');
 
     });
 
 });
 
+// Start the HTTP server.
 http.listen(port, function() {
     console.log('Listening on *:' + port);
 });
